a387f5ba2dee97faf3e839522e0b303c8bc61fb7
Junior Indart <juniorindart@gmail.com>
2018-11-14 16:58:46 +0900

    Adiciona campo pago

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from django.utils.timezone import now
-
 from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'cpf', 'created_at',
-                    'subscribed_today')
+                    'subscribed_today', 'paid')
     date_hierarchy = 'created_at'
     search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
-    list_filter = ('created_at',)
+    list_filter = ('paid', 'created_at')
 
     def subscribed_today(self, obj):
         return obj.created_at == now().date()
