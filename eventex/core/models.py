75b7339fd932b1ad79dc2a8a2fdabdfa6ecacdc2
Junior Indart <juniorindart@gmail.com>
2018-12-07 11:18:33 +0900

    Managers

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.shortcuts import resolve_url as r
 
+from eventex.core.managers import KindQuerySet, PeriodManager
+
 
 class Speaker(models.Model):
     name = models.CharField('nome', max_length=255)
@@ -34,8 +36,11 @@ class Contact(models.Model):
         verbose_name='palestrante',
         on_delete=models.CASCADE,
     )
+
     kind = models.CharField('tipo', max_length=1, choices=KINDS)
     value = models.CharField('valor', max_length=255)
+
+    objects = KindQuerySet.as_manager()
 
     class Meta:
         verbose_name = 'contato'
@@ -50,6 +55,8 @@ class Talk(models.Model):
     start = models.TimeField('inicio', blank=True, null=True)
     description = models.TextField('descrição', blank=True)
     speakers = models.ManyToManyField('Speaker', verbose_name='palestrantes', blank=True)
+
+    objects = PeriodManager()
 
     class Meta:
         verbose_name_plural = 'palestras'
--- eventex/core/tests/test_model_contact.py
+++ eventex/core/tests/test_model_contact.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from eventex.core.models import Speaker, Contact
 
 
-class ContqctmodelTest(TestCase):
+class ContactModelTest(TestCase):
     def setUp(self):
         self.speaker = Speaker.objects.create(
             name='Henrique Bastos',
@@ -24,10 +24,34 @@ class ContqctmodelTest(TestCase):
         self.assertTrue(Contact.objects.exists())
 
     def test_choices(self):
-        """Contact shoulf be limited to E or P"""
+        """Contact should be limited to E or P"""
         contact = Contact(speaker=self.speaker, kind='A', value='B')
         self.assertRaises(ValidationError, contact.full_clean)
 
     def test_str(self):
         contact = Contact(speaker=self.speaker, kind=Contact.EMAIL, value='henrique@bastos.net')
         self.assertEqual('henrique@bastos.net', str(contact))
+
+
+class ContactManagerTest(TestCase):
+    def setUp(self):
+        s = Speaker.objects.create(
+            name='Henrique Bastos',
+            slug='henrique-bastos',
+            photo='http://hbn.link/hb-pic'
+        )
+
+        s.contact_set.create(kind=Contact.EMAIL, value='henrique@bastos.net')
+        s.contact_set.create(kind=Contact.PHONE, value='21-996186180')
+
+    def test_emails(self):
+        qs = Contact.objects.emails()
+        expected = ['henrique@bastos.net']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.value)
+
+    def test_phones(self):
+        qs = Contact.objects.phones()
+        expected = ['21-996186180']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.value)
+
+
